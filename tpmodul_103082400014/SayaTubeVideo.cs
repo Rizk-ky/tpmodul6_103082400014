11705a963f06980285cef47b2f00bd329e4d400e
ravinhub <muhammadrizkyramadhan365@gmail.com>
2026-04-05 21:50:47 +0700

    menambahkan class SayaTubeVideo

--- tpmodul_103082400014/SayaTubeVideo.cs
+++ tpmodul_103082400014/SayaTubeVideo.cs
@@ -1,1 +1,49 @@
-﻿
+﻿public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (title == null)
+        {
+            throw new ArgumentNullException("title", "Judul video tidak boleh kosong");
+        }
+
+        if (title.Length > 100)
+        {
+            throw new ArgumentException("Judul video tidak boleh lebih dari 100 karakter");
+        }
+
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count > 10000000)
+        {
+            throw new ArgumentException("Penambahan play count maksimal 10.000.000 per panggilan");
+        }
+
+        if (count < 0)
+        {
+            throw new ArgumentException("Penambahan play count tidak boleh negatif");
+        }
+
+        checked
+        {
+            playCount += count;
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID Video    : " + id);
+        Console.WriteLine("Judul Video : " + title);
+        Console.WriteLine("Play Count  : " + playCount);
+    }
+}
